ebeaf1ad000c705684eed62cf7f5597ed0503ab2
qsq <hzqiushengqiang@corp.netease.com>
2016-08-10 21:29:01 +0800

    bookmark update

--- bookmark/index.js
+++ bookmark/index.js
@@ -4,32 +4,53 @@ require.config({
     }
 });
 define(['../lib/vue.min', 'text!./bookmark.html'], function (Vue, bookmark) {
-    var father = '', bookmarks = [], json, anchors = [];
-    bookmark.replace(/\<H3[^>]*\>\s*?([^<]*)\<\/H3\>|\<A[^>]*\>\s*?([^<]*)\<\/A\>/gi, function (item, title, name) {
-        if (title) {
-            if (father != title)anchors.push(title);
-            father = title;
-            bookmarks.push({father:'',name:title});
-            return;
-        }
-        if (name) {
-            var icon = '',url='';
-            item.replace(/ICON+=\"(.+?)\"/, function (a, b) {
-                icon = b;
+    var father = '', bookmarks = [], anchors = [];
+    var util = {
+        getBookmarkData: function (html) {
+            html.replace(/\<H3[^>]*\>\s*?([^<]*)\<\/H3\>|\<A[^>]*\>\s*?([^<]*)\<\/A\>/gi, function (item, title, name) {
+                if (title) {
+                    if (father != title)anchors.push({title: title, selected: false});
+                    father = title;
+                    bookmarks.push({father: '', name: title});
+                    return;
+                }
+                if (name) {
+                    var json, icon = '', url = '';
+                    item.replace(/ICON+=\"(.+?)\"/, function (a, b) {
+                        icon = b || '';
+                    });
+                    item.replace(/HREF+=\"(.+?)\"/, function (a, b) {
+                        url = b || '';
+                    });
+                    name = name.replace(/\n/, '');
+                    name = name.replace(/\s{2,10}/, ' ');
+                    json = {url: url, father: father, name: name, icon: icon};
+                    bookmarks.push(json);
+                }
             });
-            item.replace(/HREF+=\"(.+?)\"/, function (a, b) {
-                url = b;
-            });
-            name=name.replace(/\n/,'');
-            name=name.replace(/\s{2,10}/,' ');
-            if (icon && icon.length)json.icon = icon;
-            json = {url: url, father: father, icon: icon, name: name};
-            bookmarks.push(json);
+            return {bookmarks: bookmarks, anchors: anchors}
         }
-    })
+    };
+    var data = util.getBookmarkData(bookmark);
     new Vue({
         el: "#bookmark",
-        data: {bookmarks: bookmarks, anchors: anchors},
-        methods: {}
+        data: {
+            bookmarks: data.bookmarks,
+            anchors: data.anchors,
+            hasReady: true,
+            showType: 1,
+            currentAnchor:data.anchors[0].title
+        },
+        created:function(){
+          this.$data.anchors[0].selected=true;
+        },
+        methods: {
+            select: function (anchor) {
+                this.$data.anchors.map(item => {item.selected = false
+            })
+                anchor.selected = true;
+                this.$data.currentAnchor = anchor.title;
+            }
+        }
     })
 });
