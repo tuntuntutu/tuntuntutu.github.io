4399e1a52b31ff0c84ddacc4c78a6a0ee3c9663f
qsq <hzqiushengqiang@corp.netease.com>
2016-08-10 14:56:34 +0800

    bookmark update

--- bookmark/index.js
+++ bookmark/index.js
@@ -3,17 +3,31 @@ require.config({
         text: "../lib/text"
     }
 });
-define(['../lib/vue.min', 'text!./cache.html'], function (Vue, bookmark) {
-    var father = '', array = [], json;
-    bookmark.replace(/\<H3[^>]*\>\s*?([^<]*)\<\/H3\>|\<A[^>]*\>\s*?([^<]*)\<\/A\>/gi, function (a, title, name) {
+define(['../lib/vue.min', 'text!./bookmark.html'], function (Vue, bookmark) {
+    var father = '', bookmarks = [], json, anchors = [];
+    bookmark.replace(/\<H3[^>]*\>\s*?([^<]*)\<\/H3\>|\<A[^>]*\>\s*?([^<]*)\<\/A\>/gi, function (item, title, name) {
         if (title) {
+            if (father != title)anchors.push(title);
             father = title;
+            bookmarks.push({father:'',name:title});
             return;
         }
         if (name) {
-            json = {url: a.match(/HREF+=\".+?\"/)[0], father: father, name: name};
-            array.push(json);
+            var icon = '',url='';
+            item.replace(/ICON+=\"(.+?)\"/, function (a, b) {
+                icon = b;
+            });
+            item.replace(/HREF+=\"(.+?)\"/, function (a, b) {
+                url = b;
+            });
+            if (icon && icon.length)json.icon = icon;
+            json = {url: url, father: father, icon: icon, name: name};
+            bookmarks.push(json);
         }
     })
-    console.log(array)
-})
+    new Vue({
+        el: "#bookmark",
+        data: {bookmarks: bookmarks, anchors: anchors},
+        methods: {}
+    })
+});
