4798121f6592818b60c3eb173a07a7c5ee21be44
qsq <hzqiushengqiang@corp.netease.com>
2016-11-01 14:26:18 +0800

    components update

--- components/index.js
+++ components/index.js
@@ -7,16 +7,23 @@ require([
     '../lib/vue.min',
     'text!./tpl.html'
     , 'text!./widgets/button.html'
+    , 'text!./widgets/rippleButton.html'
     , 'text!./widgets/filterBox.html'
     , 'text!./widgets/tab.html'
     , 'text!./widgets/AUModal.html'
-], function (Vue, tpl, Button, FilterBox, Tab, AUModal) {
+], function (Vue, tpl, Button,RippleButton, FilterBox, Tab, AUModal) {
     var widgets = [
         {
-            name: "按钮",
+            name: "普通按钮",
             wName: "buttonBox",
             html: Button,
             current: true
+        }
+        , {
+            name: "涟漪按钮",
+            wName: "rippleButton",
+            html: RippleButton,
+            current: false
         }
         , {
             name: "列表控件",
@@ -42,6 +49,31 @@ require([
     });
     Vue.component('buttonBox', Vue.extend({
         template: Button
+    }));
+    Vue.component('rippleButton', Vue.extend({
+        template: RippleButton,
+        data(){
+            return {
+                location: {
+                    x: 0,
+                    y: 0
+                },
+                isShowAnimate:false,
+                class:'btn btn-primary'
+            }
+        },
+        methods:{
+            clickRButton(e){
+                var ripple = this.$els.ripple;
+                this.isShowAnimate=false;
+                this.location.x = parseInt(e.layerX) - (ripple.offsetWidth / 2);
+                this.location.y = parseInt(e.layerY) - (ripple.offsetHeight / 2);
+                setTimeout(function(){
+                    this.isShowAnimate=true;
+                    this.$emit('click');
+                }.bind(this),100)
+            }
+        }
     }));
     Vue.component('filterBox', Vue.extend({
         template: FilterBox,
